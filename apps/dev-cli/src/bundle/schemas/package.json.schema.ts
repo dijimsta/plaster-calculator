061c25ffe753e52665167afdb8d617c457bb1441
xixi919 <xumengxi919@gmail.com>
2026-06-13 14:30:50 +1000

    fix(dev-cli): remove workspace dev dependencies from bundle (#87)

--- apps/dev-cli/src/bundle/schemas/package.json.schema.ts
+++ apps/dev-cli/src/bundle/schemas/package.json.schema.ts
@@ -3,5 +3,6 @@ import { z } from "zod";
 export const PackageJsonSchema = z
     .object({
         dependencies: z.record(z.string(), z.string()).readonly().optional(),
+        devDependencies: z.record(z.string(), z.string()).readonly().optional(),
     })
     .readonly();
