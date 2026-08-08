44404def3ba87937275c3d8347403f3d77cbaedc
xixi919 <xumengxi919@gmail.com>
2026-08-09 07:46:13 +1000

    fix(plaster-calculator-functions): omit deploy dev dependencies (#210)

--- apps/dev-cli/src/bundle/schemas/package.json.schema.ts
+++ apps/dev-cli/src/bundle/schemas/package.json.schema.ts
@@ -3,6 +3,5 @@ import { z } from "zod";
 export const PackageJsonSchema = z
     .object({
         dependencies: z.record(z.string(), z.string()).readonly().optional(),
-        devDependencies: z.record(z.string(), z.string()).readonly().optional(),
     })
     .readonly();
